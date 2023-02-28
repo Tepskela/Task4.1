276409f9bc64c410ec676e097f6a568848bbed95
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-02-28 20:37:30 +0300

    Rename percent to Percent

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -34,6 +34,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shoulCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
